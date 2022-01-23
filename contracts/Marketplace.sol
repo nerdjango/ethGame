5df356a81a6a7826c2ede2500e95591d02af9e3b
Ifeanyichukwu Welechi <welechi@icloud.com>
2022-01-24 00:05:48 +0100

    implemented contracts

---
+++ contracts/Marketplace.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
+
+contract Marketplace is IERC1155Receiver {
+    IERC1155 private _token;
+
+    mapping(uint => uint) price;
+
+    constructor(IERC1155 token){
+        require(address(token) != address(0));
+        _token = token;
+        price[1]=100000000000000;
+        price[2]=200000000000000;
+        price[3]=300000000000000;
+    }
+
+    receive() external payable {
+        buyToken(1);
+    }
+
+    function buyToken(uint token) public payable {
+        uint weiAmount = msg.value;
+        require(weiAmount >= price[token] && price[token] != 0);
+
+        _token.safeTransferFrom(address(this), msg.sender, token, 1, "");
+    }
+
+    function onERC1155Received(
+        address operator,
+        address from,
+        uint256 id,
+        uint256 value,
+        bytes calldata data
+    ) external returns (bytes4){
+        return bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)"));
+    }
+
+    function onERC1155BatchReceived(
+        address operator,
+        address from,
+        uint256[] calldata ids,
+        uint256[] calldata values,
+        bytes calldata data
+    ) external returns (bytes4){
+        return bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"));
+    }
+}
